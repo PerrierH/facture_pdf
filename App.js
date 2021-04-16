6705fef7f15aa33e5b228a71630aabd203134dc5
PerrierH <hugues14500@gmail.com>
2021-04-16 09:55:20 +0200

    Text Input

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+
 
 export default function App() {
+  const [text, onChangeText] = React.useState(null);
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
+      <SafeAreaView>
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Nom Prenom"
+            keyboardType="text"
+        />
+        <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Prix"
+            keyboardType="numeric"
+        />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +37,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    width: 150,
+    textAlign: 'center',
   },
 });
